36ef6ed266ec72340ba486b6167c31fc9c6c1297
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2020-06-14 16:46:38 +0200

    Update plugin to set polyfill to be in head

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,25 +1,16 @@
-import React from 'react'
-
-export function onRenderBody({ setPostBodyComponents }, options){
-	let args = []
-	for (let i in options) {
-		if (i === 'plugins') continue
-		let opt = options[i]
-		if (Array.isArray(opt)) {
-			opt = opt.join(`,`)
+export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }, options) => {
+	const args = [];
+	Object.keys(options).forEach(key => {
+		if (key !== 'plugins') {
+			const opt = Array.isArray(options[key]) ? options[key].join(',') : options[key];
+			args.push(`${key}=${opt}`);
 		}
-		args.push(`${i}=${opt}`)
-	}
-	if (args.length) {
-		args = `?${args.join(`&`)}`
-	}
-	else {
-		args = ``
-	}
-	setPostBodyComponents([
+	});
+	const parameters = (args.length > 0) ? `?${args.join('&')}` : '';
+	setHeadComponents([
 		<script
 			key='polyfill-io'
-			src={`https://cdn.polyfill.io/v3/polyfill.min.js${args}`}
+			src={`https://cdn.polyfill.io/v3/polyfill.min.js${parameters}`}
 		/>
 	])
 }
